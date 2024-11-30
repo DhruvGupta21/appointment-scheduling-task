29cd96f5d96ea829389595dff08164b00c359482
DhruvGupta21 <dhruvgenie21@gmail.com>
2024-11-30 23:24:54 +0530

    comments added to verifyRoutes.ts

--- dist/routes/verifyRoutes.js
+++ dist/routes/verifyRoutes.js
@@ -1,26 +1,31 @@
 import { Router } from "express";
 import prisma from "../config/database.js";
 const router = Router();
+//route for verification email link
 router.get("/verify-email", async (req, res) => {
     const { email, token } = req.query;
+    //check if email and token both exists
     if (email && token) {
+        //find the user with given email
         const user = await prisma.user.findUnique({
             where: {
                 email: email
             }
         });
+        //check if user exists
         if (user) {
+            //check if the token passed is same as email verify token
             if (token === user.email_verify_token) {
-                //redirect to success page
                 await prisma.user.update({
                     data: {
-                        email_verify_token: null,
-                        email_verified_at: new Date().toISOString()
+                        email_verify_token: null, //reset token to null to avoid misuse and multiple verifications
+                        email_verified_at: new Date().toISOString() //add time of verification to database
                     },
                     where: {
                         email: email
                     }
                 });
+                //redirect to success page
                 return res.redirect("/verify-success");
             }
         }
@@ -28,9 +33,11 @@ router.get("/verify-email", async (req, res) => {
     }
     return res.redirect("/verify-error");
 });
+//route for verification error page
 router.get("/verify-error", async (req, res) => {
     return res.render("auth/emailVerifyError");
 });
+//route for verification success page
 router.get("/verify-success", async (req, res) => {
     return res.render("auth/emailVerifySuccess");
 });
--- src/routes/verifyRoutes.ts
+++ src/routes/verifyRoutes.ts
@@ -2,26 +2,33 @@ import { Router, Response, Request } from "express";
 import prisma from "../config/database.js";
 const router = Router();
 
+//route for verification email link
 router.get("/verify-email", async (req: Request, res: Response) => {
     const { email, token } = req.query;
+
+    //check if email and token both exists
     if (email && token) {
+        //find the user with given email
         const user = await prisma.user.findUnique({
             where: {
                 email: email as string
             }
         });
+        //check if user exists
         if (user) {
+            //check if the token passed is same as email verify token
             if (token === user.email_verify_token) {
-                //redirect to success page
+
                 await prisma.user.update({
                     data: {
-                        email_verify_token: null,
-                        email_verified_at: new Date().toISOString()
+                        email_verify_token: null, //reset token to null to avoid misuse and multiple verifications
+                        email_verified_at: new Date().toISOString() //add time of verification to database
                     },
                     where: {
                         email: email as string
                     }
                 });
+                //redirect to success page
                 return res.redirect("/verify-success");
             }
         }
@@ -30,10 +37,12 @@ router.get("/verify-email", async (req: Request, res: Response) => {
     return res.redirect("/verify-error");
 });
 
+//route for verification error page
 router.get("/verify-error", async (req: Request, res: Response) => {
     return res.render("auth/emailVerifyError");
 });
 
+//route for verification success page
 router.get("/verify-success", async (req: Request, res: Response) => {
     return res.render("auth/emailVerifySuccess");
 });
