a862c090e7142326eaa226bf5688fd35444feb69
DhruvGupta21 <dhruvgenie21@gmail.com>
2024-11-30 23:28:06 +0530

    added comments to appointment schema validation

--- src/validation/appointmentValidation.ts
+++ src/validation/appointmentValidation.ts
@@ -1,4 +1,6 @@
 import { z } from "zod"
+
+//schema validation for appointment
 export const appointmentSchema = z.object({
     date: z.string({ message: "Date must be provided." })
         .regex(/^\d{4}-\d{2}-\d{2}$/, { message: "Date must be in YYYY-MM-DD format." })
